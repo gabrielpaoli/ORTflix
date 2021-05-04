3a07979aa85971b2701ae83253fa541f7038040a
gabrielpaoli <gabrielpaoli@hotmail.com>
2021-05-03 23:35:22 -0300

    Add movies section.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace ORTflix.Controllers
             return View();
         }
 
+        public IActionResult Peliculas()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
