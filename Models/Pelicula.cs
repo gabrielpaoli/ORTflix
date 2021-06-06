e935cca284ebc7b616be7050eab6fcc4992e110c
gabrielpaoli <gabrielpaoli@hotmail.com>
2021-06-06 17:45:30 -0300

    feature peliculas

---
+++ Models/Pelicula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ORTflix.Models
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public int Nombre { get; set; }
+        public int ano { get; set; }
+
+    }
+}
